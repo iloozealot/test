04f7ff7f636f25e05b3e0e9efc68eb6afcc867b4
iloozealot <iloozealot@gmail.com>
2023-11-06 21:28:01 -0600

    Initial commit

---
+++ Test/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Justin!");
+Console.WriteLine("What is your favorite color?");
+string? input = Console.ReadLine();
+Console.WriteLine($"{input} is my favorite color too!");
